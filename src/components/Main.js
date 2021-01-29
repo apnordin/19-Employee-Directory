90f5492e875e89213265442080285ca0504cd4ed
Alex Nordin <apnordin@gmail.com>
2021-01-28 21:48:07 -0500

    Added reactbootstrap and learned how to add table

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,18 +7,24 @@ import API from "../utils/API";
 class Main extends React.Component {
     state = {
         result: [],
-        search: ''
+        search: "",
     }
 
     //When the component mounts, get a list of 20 random employees
     componentDidMount() {
         API.getUsers()
         .then(res => {
+            console.log('response: ', res);
             this.setState({ result: res.data.results });
-            console.log(this.state.result);
+            console.log('this.state.result: ', this.state.result);
         })
         .catch(err => console.log(err));
     }
+
+    // handleInputChange = event => {
+    //     this.setState({ search: event.target });
+    //     console.log('event.target: ', event.target)
+    // }
 
     render() {
         return(
--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,16 +1,33 @@
 import React from "react";
+import Table from 'react-bootstrap/Table'
 
-function UsersList(props) {
+function UsersList(props, bordered) {
+
+    //Reference https://react-bootstrap.github.io/components/table/
 
     return (
-        <ul className="list-group">
-            {/* {props.results.map(result => (
-                <li key={result} className="list-group-item"> */}
-                    <li>Placeholderfornow</li>
-                {/* </li>
-            ))} */}
-        </ul>
+        <Table striped bordered hover className="userTable">
+            <thead>
+                <tr>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Phone</th>
+                    <th>Email</th>
+                    <th>DOB</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>placeholder</td>
+                <td>Test Name</td>
+                <td>919-555-5555</td>
+                <td>test@gmail.com</td>
+                <td>08-01-1992</td>
+                </tr>
+            </tbody>
+        </Table>
     );
 }
 
 export default UsersList;
+
